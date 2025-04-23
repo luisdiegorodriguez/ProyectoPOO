eff3b413873540228879b9ec54c5587f03a499f5
Emily0523 <hemily2310@gmail.com>
2025-04-23 14:30:49 -0600

    Main

--- Proyecto/src/proyecto/main.java
+++ Proyecto/src/proyecto/main.java
@@ -9,17 +9,19 @@ import javax.swing.JOptionPane;
 public class main {
    
     public static void main(String[] args) {         
-        ClienteFactura.menuFactura();
         RegistroUsuarios registro = new RegistroUsuarios();
         while (true) {
-            String opcion = JOptionPane.showInputDialog("1. Agregar\n2. Consultar\n3. Inactivar\n4. Salir");
+            String opcion = JOptionPane.showInputDialog("1. Agregar usuario\n2. Consultar lista de usuarios registrados\n3. Inactivar usuario\n4. Generar o cancelar factura a un usuario\n5. Salir");
             switch (opcion) {
                 case "1": registro.agregarUsuario(); break;
                 case "2": registro.consultarUsuarios(); break;
                 case "3": registro.eliminarUsuario(); break;
-                case "4": JOptionPane.showMessageDialog(null, "Ha salido del programa.");
+                case "4": ClienteFactura.menuFactura(); break;
+                case "5": JOptionPane.showMessageDialog(null, "Ha salido del programa.");
                     System.exit(0);
                 default: JOptionPane.showMessageDialog(null, "Opción inválida.");
+                
+                
             }
         }    
         
